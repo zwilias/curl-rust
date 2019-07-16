ded1a1b9e6213a27d5e3231e32b2a838504b092b
Deploy from CI <>
2019-07-16 15:31:38 +0000

    Deploy 0ba03a4790710425515e8177591816d185ad73a6 to gh-pages

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["curl"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"curl/easy/enum.ProxyType.html\" title=\"enum curl::easy::ProxyType\">ProxyType</a>",synthetic:false,types:["curl::easy::handler::ProxyType"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
